28d1610b94abaa5aae48221fc65395b7b678482f
anuragdhingra <anuragdhingra101@gmail.com>
2019-02-04 01:43:45 +0900

    some modifications in the script

--- scripts/run-gocheckstyle.sh
+++ scripts/run-gocheckstyle.sh
@@ -3,7 +3,7 @@ set -v
 if [ "${CIRCLE_BRANCH}" != "master" ]; then
   # Circle-CI
   #
-  sudo apt-get install && sudo apt-get install rubygems
+  sudo apt-get update && sudo apt-get install rubygems
   sudo gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
 
 
@@ -21,7 +21,6 @@ if [ "${CIRCLE_BRANCH}" != "master" ]; then
 echo "********************"
 echo "* checkstyle       *"
 echo "********************"
-gocheckstyle -reporter=xml -config=.gostyle 2> gostyle.xml
 cat gostyle.xml \
     | checkstyle_filter-git diff origin/master \
     | saddler report --require saddler/reporter/github --reporter $REPORTER
